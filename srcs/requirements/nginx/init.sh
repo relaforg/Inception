bb5d90995309d4e49938b1d2f58f39e398346ab5
relaforg <relaforg@Inception.relaforg.42.fr>
2026-04-21 16:18:32 +0200

    Fix ssl duration

--- srcs/requirements/nginx/init.sh
+++ srcs/requirements/nginx/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p /etc/nginx/ssl
-openssl req -x509 -nodes -days 265 -newkey rsa:2048 \
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 	-keyout /etc/nginx/ssl/nginx.key \
 	-out /etc/nginx/ssl/nginx.crt \
 	-subj "/CN=relaforg.42.fr"
